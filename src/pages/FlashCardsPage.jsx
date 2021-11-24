10ab22c9ecd270315c5100335bece04c39591993
Alexandre Kimura <alexandremkimura@hotmail.com>
2021-11-23 23:37:15 -0300

    git initial

---
+++ src/pages/FlashCardsPage.jsx
@@ -0,0 +1,14 @@
+import FlashCard from '../components/FlashCard'
+import Header from '../components/Header'
+import Main from '../components/Main'
+
+export default function FlashCardsPage() {
+  return(
+    <>
+      <Header>react-flash-cards-v1</Header>
+      <Main>
+        <FlashCard />
+      </Main>
+    </>
+  )
+}

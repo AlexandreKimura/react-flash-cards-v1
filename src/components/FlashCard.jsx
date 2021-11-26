ab0a9e5df244be611e189c64fc55934067c7a3e9
Alexandre Kimura <alexandremkimura@hotmail.com>
2021-11-25 22:43:11 -0300

    Create component FlashCard

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -1,10 +1,23 @@
-export default function FlashCard() {
+import { useState } from "react"
+
+export default function FlashCard({ title = 'Título do card', description = 'Descrição do card, que pode conter mais palavras que o título' }) {
+  
+  const [showTitle, setShowTitle] = useState(true)
+
+  const fontSizeClassName = showTitle ? 'text-xl' : 'text-md'
+
+  function handleCardClick() {
+    setShowTitle(currentShowTitle => !currentShowTitle)
+  }
+
   return (
-    <div className={`border p-4 w-64 h-32 
+    <div className={`shadow-lg p-4 w-64 h-32 cursor-pointer
       flex flex-row items-center justify-center
-      font-semibold text-xl`} 
-      style={{fontFamily: "'JetBrains Mono', monospace"}}>
-      FlashCard
+      font-semibold ${fontSizeClassName}`} 
+      style={{fontFamily: "'JetBrains Mono', monospace"}}
+      onClick={handleCardClick}
+      >
+      {showTitle ? title : description}
     </div>
   )
 }
--- src/pages/FlashCardsPage.jsx
+++ src/pages/FlashCardsPage.jsx
@@ -1,6 +1,7 @@
 import FlashCard from '../components/FlashCard'
 import Header from '../components/Header'
 import Main from '../components/Main'
+import { allFlashCards } from '../data/flashcards'
 
 export default function FlashCardsPage() {
   return(
@@ -8,6 +9,7 @@ export default function FlashCardsPage() {
       <Header>react-flash-cards-v1</Header>
       <Main>
         <FlashCard />
+        <FlashCard title="React" description="Biblioteca para o Front End"/>
       </Main>
     </>
   )
